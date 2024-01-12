78b992197303feeee0aac216b9226949a749f45c
edymol <edymolinap@gmail.com>
2024-01-12 10:12:54 +0000

    remove: locations

--- location/src/main/java/xyz/edydev/location/repos/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.edydev.location.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import xyz.edydev.location.entities.Location;
-
-public interface LocationRepository extends JpaRepository<Location, Integer> {
-
-}
