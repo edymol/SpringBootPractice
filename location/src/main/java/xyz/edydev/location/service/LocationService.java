196b6f2be06740e4c7475bd3605747733f32c5d7
edymol <edymolinap@gmail.com>
2024-01-12 17:30:41 +0000

    fix: service class

--- location/src/main/java/xyz/edydev/location/service/LocationService.java
+++ location/src/main/java/xyz/edydev/location/service/LocationService.java
@@ -2,8 +2,11 @@ package xyz.edydev.location.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import xyz.edydev.location.entities.Location;
 
+@Service
 public interface LocationService {
 	Location saveLocation (Location location);
 	Location updateLocation (Location location);
